73149adbfe8b1f2581921eb5be14675b3cefafe8
tingtt <t.t.launcher.real@gmail.com>
2021-06-06 15:42:50 +0900

    アプリケーション画面のLayoutコンポーネントとNavigationコンポーネントのファイル作成

---
+++ components/app/Layout.tsx
@@ -0,0 +1,7 @@
+import Navigation from './Navigation'
+
+export default function Layout() {
+    return (
+        <Navigation />
+    );
+}
--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -1,10 +1,16 @@
+//認証失敗時や未ログインじにLoginページにリダイレクト
+// import Router from "next/router";
+// const redirectToLoginPage = () => {
+//     Router.push('/login');
+// }
 
+import Layout from "../../components/app/Layout";
 
 const appIndex = () => {
-    return <div>
-        <p>ログイン認証に成功したらアプリケーション画面を開く</p>
-        <p>未ログインの場合はログイン画面にリダイレクト</p>
-    </div>;
+    return (
+        <Layout />
+        // <button onClick={redirectToLoginPage}>認証失敗時動作</button>
+    );
 }
 
 export default appIndex
