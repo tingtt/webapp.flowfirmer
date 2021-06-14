b19c7aeb5c9bbc72b0fb6821a75ada0dab5f3bbd
tingtt <t.t.launcher.real@gmail.com>
2021-06-14 18:01:08 +0900

    冗長コードの削除

--- components/app/NavigationDrawer.tsx
+++ components/app/NavigationDrawer.tsx
@@ -5,7 +5,6 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/Inbox';
 
 import { NavigationState } from '../../lib/state/navigationState'
 import { ClassNameMap } from "@material-ui/styles";
