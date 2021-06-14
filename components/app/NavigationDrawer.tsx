e64a308525f16dd5b04ad06e9f5a585bd7d72eed
tingtt <t.t.launcher.real@gmail.com>
2021-06-14 17:47:40 +0900

    コードの可読性の改善、アイコンの変更

--- components/app/NavigationDrawer.tsx
+++ components/app/NavigationDrawer.tsx
@@ -5,11 +5,12 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
+import InboxIcon from '@material-ui/icons/Inbox';
 
 import { NavigationState } from '../../lib/state/navigationState'
 import { ClassNameMap } from "@material-ui/styles";
 import AppNavigatoinListManager from "../../lib/app/appNavigationListManager";
+import { AccountCircle, AddCircle, Bookmark, BookmarkBorder, Search, Today, ViewWeek, AllInbox, Dashboard } from "@material-ui/icons";
 
 type Props = {
 	drawerOpen: boolean
@@ -35,39 +36,43 @@ export default function NavigationDrawer(props: Props) {
         >
         <Divider />
         <List>
-            {['Profile', 'New', 'Search'].map((text) => (
-                <ListItem button key={text}>
-                <ListItemIcon><InboxIcon/></ListItemIcon>
-                <ListItemText primary={text} />
-                </ListItem>
-            ))}
+            <ListItem button key={'Profile'}>
+                <ListItemIcon><AccountCircle/></ListItemIcon>
+                <ListItemText primary={'Profile'}/>
+            </ListItem>
+            <ListItem button key={'New'}>
+                <ListItemIcon><AddCircle/></ListItemIcon>
+                <ListItemText primary={'New'}/>
+            </ListItem>
+            <ListItem button key={'Search'}>
+                <ListItemIcon><Search/></ListItemIcon>
+                <ListItemText primary={'Search'}/>
+            </ListItem>
         </List>
         <Divider />
         <List>
             {appNavigationManager.navigationListItems.originalItems.map((navigationState) => (
                 <ListItem button key={navigationState.name} onClick={()=>props.setNav(navigationState)}>
-                    <ListItemIcon><InboxIcon/></ListItemIcon>
+                    <ListItemIcon>
+                        {navigationState.name == 'All' && <AllInbox/>}
+                        {navigationState.name == 'Today' && <Today/>}
+                        {navigationState.name == 'Weekly' && <ViewWeek/>}
+                        {navigationState.name == 'Dashboard' && <Dashboard/>}
+                    </ListItemIcon>
                     <ListItemText primary={navigationState.name} />
                 </ListItem>
             ))}
         </List>
         <Divider />
         <List>
-            {appNavigationManager.navigationListItems.pinnedTargets.map((navigationState) => {
-                const keyStr = (navigationState.name == 'Target') ? navigationState.target.name : "";
+            {[...appNavigationManager.navigationListItems.pinnedTargets, ...appNavigationManager.navigationListItems.otherTargets].map((navigationState) => {
+                if (navigationState.name != 'Target') {
+                    throw new Error("NavigatoinState is not correct.");
+                }
                 return (
-                    <ListItem button key={keyStr} onClick={()=>props.setNav(navigationState)}>
-                        <ListItemIcon><InboxIcon/></ListItemIcon>
-                        <ListItemText primary={keyStr} />
-                    </ListItem>
-                );
-            })}
-            {appNavigationManager.navigationListItems.otherTargets.map((navigationState) => {
-                const keyStr = (navigationState.name == 'Target') ? navigationState.target.name : "";
-                return (
-                    <ListItem button key={keyStr} onClick={()=>props.setNav(navigationState)}>
-                        <ListItemIcon><InboxIcon/></ListItemIcon>
-                        <ListItemText primary={keyStr} />
+                    <ListItem button key={navigationState.target.name} onClick={()=>props.setNav(navigationState)}>
+                        <ListItemIcon>{(navigationState.target.pinnedAtNavigationList) ? <Bookmark/> : <BookmarkBorder/>}</ListItemIcon>
+                        <ListItemText primary={navigationState.target.name} />
                     </ListItem>
                 );
             })}
