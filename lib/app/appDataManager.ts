980ab825e3ad5f9b5acc65560da903e12755359a
tingtt <t.t.launcher.real@gmail.com>
2021-06-17 09:31:35 +0900

    ユーザーのデータを管理するクラス（データ取得のみ実装済）

---
+++ lib/app/appDataManager.ts
@@ -0,0 +1,60 @@
+import { sampleHabitReminds, sampleTargets, sampleTerms, sampleToDos } from "../../utils/sample-data";
+import { HabitRemind, Target, Term, ToDo } from "../interface/index";
+
+export default class AppDataManager {
+
+    private static _instance: AppDataManager
+
+    public static generateInstance(user_id: number): AppDataManager {
+        // インスタンスが既に生成されている場合にエラー
+        if (this._instance) {
+            throw new Error("AppDataManager instance already exists.");
+        }
+
+        console.log("Generating 'AppDataManager' instance.");
+        this._instance = new AppDataManager(user_id);
+
+        return this._instance;
+    }
+
+    public static getInstance(): AppDataManager {
+        if (!this._instance) {
+            throw new Error("'AppDataManager' instance doesn't exist.");
+        }
+        return this._instance;
+    }
+
+    private user_id: number;
+
+    public targets?: Target[];
+    public todos?: ToDo[];
+    public terms?: Term[];
+    public habitReminds?: HabitRemind[];
+
+    // TODO: ユーザーの登録データを取得
+
+    private getTargets() {
+        return sampleTargets.filter(value => value.user_id == this.user_id);
+    }
+
+    private getToDos() {
+        return sampleToDos.filter(value => value.user_id == this.user_id);
+    }
+
+    private getTerms() {
+        return sampleTerms.filter(value => value.user_id == this.user_id);
+    }
+
+    private getHabitReminds() {
+        return sampleHabitReminds.filter(value => value.user_id == this.user_id);
+    }
+
+    private constructor(user_id: number) {
+        this.user_id = user_id;
+
+        this.targets = this.getTargets();
+        this.todos = this.getToDos();
+        this.terms = this.getTerms();
+        this.habitReminds = this.getHabitReminds();
+    }
+}
