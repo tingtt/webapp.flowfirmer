db54bf45ee29edad2e62ba5485ed42654dc4f386
tingtt <t.t.launcher.real@gmail.com>
2021-06-20 19:03:20 +0900

    ToDoリストの要素の構造を変更（overflowスクロールの処理がいまいちだけどとりあえず）

--- components/app/lib/ToDoListItem.tsx
+++ components/app/lib/ToDoListItem.tsx
@@ -1,4 +1,4 @@
-import { createStyles, Theme, makeStyles, Checkbox } from "@material-ui/core";
+import { createStyles, Theme, makeStyles, Checkbox, Chip } from "@material-ui/core";
 import React from "react";
 import clsx from 'clsx';
 import { ToDo } from "../../../lib/interface";
@@ -12,56 +12,73 @@ const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         root: {
             display: 'flex',
-            alignItems: 'center',
             height: theme.spacing(8),
         },
         rootHeightLong: {
             height: theme.spacing(10),
         },
-        checkBox: {
-            height: theme.spacing(5),
-            width: theme.spacing(5)
-        },
-        mainInfoSpan: {
+        infoDiv: {
             display: 'flex',
             flexDirection: 'column',
-            flexWrap: 'wrap',
-            height: '100%',
-            whiteSpace: 'nowrap',
-            overflow: 'auto',
+            width: '100%',
+        },
+        infoTopHalf: {
+            flex: 2,
+            display: 'flex',
+        },
+        infoBottomHalf: {
+            flex: 1,
+            display: 'flex',
+        },
+        infoBottomHalfLongHeight: {
+            flex: 2,
+        },
+        checkBox: {
+            height: theme.spacing(5),
+            width: theme.spacing(5),
+            marginTop: 'auto',
+            marginBottom: 'auto',
         },
         todoNameSpan: {
             flex: 4,
             display: 'flex',
             alignItems: 'center',
             fontSize: theme.spacing(2.4),
+            whiteSpace: 'nowrap',
+            overflow: 'auto',
+            msOverflowStyle: 'none',    /* IE, Edge 対応 */
+            scrollbarWidth: 'none', /* Firefox 対応 */
+            "&::-webkit-scrollbar": {  /* Chrome, Safari 対応 */
+                display: 'none',
+            },
         },
         todoDescriptionSpan: {
             flex: 0.5,
+            display: 'flex',
             fontSize: theme.spacing(1.5),
+            whiteSpace: 'nowrap',
+            overflow: 'auto',
+            msOverflowStyle: 'none',    /* IE, Edge 対応 */
+            scrollbarWidth: 'none', /* Firefox 対応 */
+            "&::-webkit-scrollbar": {  /* Chrome, Safari 対応 */
+                display: 'none',
+            },
         },
         todoTargetList: {
             flex: 1,
+            display: 'flex',
+            whiteSpace: 'nowrap',
+            overflow: 'auto',
+            msOverflowStyle: 'none',    /* IE, Edge 対応 */
+            scrollbarWidth: 'none', /* Firefox 対応 */
+            "&::-webkit-scrollbar": {  /* Chrome, Safari 対応 */
+                display: 'none',
+            },
         },
         detailInfoDiv: {
             display: 'flex',
-            flexDirection: 'column',
-            flexWrap: 'wrap',
-            marginLeft: 'auto',
-            height: '100%',
-            whiteSpace: 'nowrap'
-        },
-        detailInfoTopHalf: {
-            flex: 1,
-            display: 'flex',
             marginLeft: 'auto',
             alignItems: 'center',
-        },
-        detailInfoBottomHalf: {
-            flex: 1,
-            display: 'flex',
-            marginLeft: 'auto',
-            alignItems: 'center'
         },
         detailInfoSpan: {
             marginLeft: theme.spacing(1),
@@ -71,6 +88,10 @@ const useStyles = makeStyles((theme: Theme) =>
                 textDecoration: 'underline',
                 cursor: 'pointer',
             },
+            whiteSpace: 'nowrap',
+        },
+        sampleOverflow: {
+            overflow: 'auto',
         }
     }),
 );
@@ -92,64 +113,92 @@ export default function ToDoListItem(props: Props) {
                 [classes.rootHeightLong]: props.todo.description != undefined && props.todo.description != ""
             })}
         >
-            <Checkbox
-                className={classes.checkBox}
-                checked={checked}
-                onChange={handleChange}
-            />
-            <div
-                className={classes.mainInfoSpan}
-            >
-                {/* name */}
-                <span
-                    className={classes.todoNameSpan}
-                >
-                    {props.todo.name}{[...Array(30)].map(_ => "a")}
-                </span>
 
-                {/* Description */}
-                {props.todo.description != undefined && props.todo.description != "" &&
-                    <span
-                        className={classes.todoDescriptionSpan}
-                    >
-                        {props.todo.description}
-                    </span>
-                }
-
-                {/* TargetList */}
+            <div>
                 <div
-                    className={classes.todoTargetList}
+                    className={classes.infoTopHalf}
                 >
-                    {props.todo.targetList?.map(value => <span>#{value.name}</span>)}
+                    <Checkbox
+                        className={classes.checkBox}
+                        checked={checked}
+                        onChange={handleChange}
+                    />
                 </div>
+                <div
+                    className={clsx(classes.infoBottomHalf, {
+                        [classes.infoBottomHalfLongHeight]: props.todo.description != undefined && props.todo.description != ""
+                    })}
+                ></div>
             </div>
             <div
-                className={classes.detailInfoDiv}
+                className={classes.infoDiv}
             >
-                <span
-                    className={classes.detailInfoTopHalf}
+                <div
+                    className={classes.infoTopHalf}
                 >
-                    {props.todo.processingTimeScheduled != undefined && props.todo.processingTimeScheduled >= 60 ?
-                        <span className={classes.detailInfoSpan}>{Math.trunc(props.todo.processingTimeScheduled / 60)}h{props.todo.processingTimeScheduled % 60}min</span>
-                        :
-                        <span className={classes.detailInfoSpan}>{props.todo.processingTimeScheduled}min</span>
-                    }
-                    <span className={classes.detailInfoSpan}>6:30</span>
-                </span>
-                <span
-                    className={classes.detailInfoBottomHalf}
+                    <div
+                        className={classes.todoNameSpan}
+                    >
+                        <span>{props.todo.name}</span>
+                        {[...Array(30)].map(_ => "a")}
+                    </div>
+                    <div
+                        className={classes.detailInfoDiv}
+                    >
+                        {props.todo.processingTimeScheduled != undefined && props.todo.processingTimeScheduled >= 60 ?
+                            <span className={classes.detailInfoSpan}>{Math.trunc(props.todo.processingTimeScheduled / 60)}h{props.todo.processingTimeScheduled % 60}min</span>
+                            :
+                            <span className={classes.detailInfoSpan}>{props.todo.processingTimeScheduled}min</span>
+                        }
+                        <span className={classes.detailInfoSpan}>6:30</span>
+                    </div>
+                </div>
+                <div
+                    className={clsx(classes.infoBottomHalf, {
+                        [classes.infoBottomHalfLongHeight]: props.todo.description != undefined && props.todo.description != ""
+                    })}
                 >
-                    {(() => {
-                        if (props.todo.term != undefined) {
-                            return <span className={clsx(classes.detailInfoSpan, classes.termSpan)}>{props.todo.term.name}</span>
+                    <div
+                        className={classes.sampleOverflow}
+                    >
+                        {/* Description */}
+                        {props.todo.description != undefined && props.todo.description != "" &&
+                            <div
+                                className={classes.todoDescriptionSpan}
+                            >
+                                <span>{props.todo.description}</span>
+                            </div>
                         }
-                    })()}
-                    {(() => {
-                        if (props.todo.repeatPattern != undefined) {
-                            return <span className={classes.detailInfoSpan}><Loop /></span>
-                        }
-                    })()}
-                </span>
+
+                        {/* TargetList */}
+                        <div
+                            className={classes.todoTargetList}
+                        >
+                            <span>
+                                {props.todo.targetList?.map(value => (
+                                    // <Chip
+                                    //     label={"#" + value.name}
+                                    // />
+                                    <span>#{value.name}</span>
+                                ))}
+                            </span>
+                        </div>
+                    </div>
+                    <div
+                        className={classes.detailInfoDiv}
+                    >
+                        {(() => {
+                            if (props.todo.term != undefined) {
+                                return <span className={clsx(classes.detailInfoSpan, classes.termSpan)}>{props.todo.term.name}</span>
+                            }
+                        })()}
+                        {(() => {
+                            if (props.todo.repeatPattern != undefined) {
+                                return <span className={classes.detailInfoSpan}><Loop /></span>
+                            }
+                        })()}
+                    </div>
+                </div>
             </div>
         </div>
     );
