4d86d6681e849d428e07985cb98e8a93ce774c70
tingtt <t.t.launcher.real@gmail.com>
2021-08-18 07:47:47 +0900

    [add] ログイン処理 with token

--- components/app/Login/login.tsx
+++ components/app/Login/login.tsx
@@ -1,6 +1,9 @@
+import React from 'react';
 import Link from 'next/link'
+import { useRouter } from 'next/dist/client/router';
 import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
 import { Button, TextField } from '@material-ui/core';
+import axios from 'axios';
 
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -22,9 +25,16 @@ const useStyles = makeStyles((theme: Theme) =>
         passwordTextField: {
             marginTop: theme.spacing(2),
         },
+        msgField: {
+            height: theme.spacing(6),
+            display: 'flex',
+            "& > span": {
+                marginTop: 'auto',
+                color: theme.palette.error.main
+            }
+        },
         signInButton: {
             height: theme.spacing(6),
-            marginTop: theme.spacing(6),
             marginBottom: theme.spacing(2),
         }
     })
@@ -32,7 +42,59 @@ const useStyles = makeStyles((theme: Theme) =>
 
 export default function loginComponent() {
 
+    const router = useRouter();
+
     const classes = useStyles();
+
+    const [email, setEmail] = React.useState("");
+    const [pass, setPass] = React.useState("");
+
+    const [message, setMessage] = React.useState("");
+
+    // SignInボタンの切り替え
+    const [buttonDisabled, setButtonDisabled] = React.useState(true);
+
+    const checkEntries = (entryEmail?: string, entryPass?: string) => {
+        if (entryEmail == undefined) {
+            entryEmail = email;
+        }
+        if (entryPass == undefined) {
+            entryPass = pass;
+        }
+        if (entryEmail == "" || entryPass == "" || !/^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]{1,}.[A-Za-z0-9]{1,}$/.test(entryEmail)) {
+            setButtonDisabled(true);
+        } else {
+            setButtonDisabled(false);
+        }
+    }
+
+    const verify = (email: string, pass: string) => {
+        var args = {
+            headers: {
+                "Content-Type": "application/json",
+            },
+            email: email,
+            password: pass,
+        };
+
+        axios.post("http://localhost/api/login", args)
+            .then(function (response) {
+                if (response.data.status == 200 && typeof response.data.token === "string") {
+                    router.push(
+                        {
+                            pathname: "/app",
+                            query: { token: response.data.token },
+                        },
+                        "/app"
+                    );
+                } else {
+                    setMessage(response.data["message"]);
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
 
     return (
         <div
@@ -46,17 +108,37 @@ export default function loginComponent() {
                 className={classes.mailField}
                 variant='outlined'
                 placeholder='Email'
+                value={email}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                    setEmail(e.target.value);
+                    checkEntries(e.target.value, pass);
+                }}
             />
             <TextField
                 className={classes.passwordTextField}
                 variant='outlined'
                 placeholder='Password'
                 type='password'
+                value={pass}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                    setPass(e.target.value);
+                    checkEntries(email, e.target.value);
+                }}
+                onKeyPress={(e: React.KeyboardEvent<HTMLInputElement>) => {
+                    if (e.key === 'Enter' && !buttonDisabled) {
+                        verify(email, pass);
+                    }
+                }}
             />
+            <div className={classes.msgField}>
+                <span>{message}</span>
+            </div>
             <Button
                 className={classes.signInButton}
                 variant="contained"
                 color="primary"
+                onClick={() => verify(email, pass)}
+                disabled={buttonDisabled}
             >
                 Sign in
             </Button>
