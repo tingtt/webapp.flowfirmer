4d1c444593ef8dce4e39bb0640fb44aa08c2e30a
tingtt <t.t.launcher.real@gmail.com>
2021-08-19 07:45:31 +0900

    [add] トークン無しで'/app'にアクセスした場合のリダイレクト処理

--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -1,16 +1,22 @@
-//認証失敗時や未ログインじにLoginページにリダイレクト
-// import Router from "next/router";
-// const redirectToLoginPage = () => {
-//     Router.push('/login');
-// }
-
+import { GetServerSideProps, NextPage } from "next";
 import Layout from "../../components/app/Layout";
 
-const appIndex = () => {
+const appIndex: NextPage = () => {
     return (
         <Layout />
-        // <button onClick={redirectToLoginPage}>認証失敗時動作</button>
     );
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    if (context.res) {
+        // Without token makes page redirection.
+        if (context.query.token == undefined || context.query.token == "") {
+            context.res.statusCode = 302;
+            context.res.setHeader('location', '/login');
+            context.res.end();
+        }
+    }
+    return { props: {} };
 }
 
 export default appIndex
