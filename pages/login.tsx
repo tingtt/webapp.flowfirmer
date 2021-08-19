25782cd21f05acc9dfd16b03c9cd30a5356ca78b
tingtt <t.t.launcher.real@gmail.com>
2021-08-19 14:09:07 +0900

    [clean] 不要なコードの削除

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,20 +1,8 @@
-import Link from 'next/link'
 import LoginComponent from '../components/app/Login/login'
 
 const loginPage = () => (
   <div>
     <LoginComponent />
-    {/* <h1>Login 👋</h1>
-    <p>
-      <Link href="/">
-        <a>Index</a>
-      </Link>
-    </p>
-    <p>
-      <Link href="/app">
-        <a>Open web app.</a>
-      </Link>
-    </p> */}
   </div>
 )
 
