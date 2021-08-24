7c0449cd0dd05fca2603507657dfdb4ac31d95b2
tingtt <t.t.launcher.real@gmail.com>
2021-08-24 15:36:33 +0900

    [add] tokenの保持と'/app'にルーティングしたときのtoken所持チェック

--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -1,22 +1,34 @@
-import { GetServerSideProps, NextPage } from "next";
+import { GetServerSideProps } from "next";
 import Layout from "../../components/app/Layout";
 
-const appIndex: NextPage = () => {
+interface ServerSideIndexProps{
+    token: string
+}
+
+const appIndex = ({ token }: ServerSideIndexProps) => {
+    console.log(`appIndex: ${token}`);
+    // TODO: appDataManagerにTokenを渡して検証
     return (
         <Layout />
     );
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
+    var token: string = "";
     if (context.res) {
         // Without token makes page redirection.
-        if (context.query.token == undefined || context.query.token == "") {
+        if ((context.query.token == undefined || context.query.token == "") && (context.req.headers.cookie == undefined || context.req.headers.cookie == "" || !context.req.headers.cookie.includes("token="))) {
             context.res.statusCode = 302;
             context.res.setHeader('location', '/login');
             context.res.end();
+        } else {
+            token = typeof context.query.token === "string" && context.query.token != "" ?
+                context.query.token
+                :
+                context.req.headers.cookie!.split('; ').find((row: string) => row.startsWith('token'))!.split('=')[1];
         }
     }
-    return { props: {} };
+    return { props: { token } };
 }
 
 export default appIndex
